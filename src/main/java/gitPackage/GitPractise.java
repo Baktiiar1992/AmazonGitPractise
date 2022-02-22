13c64a429dbfb1eac78b525f7e66615463307637
Baha <bsooronbaev@gmail.com>
2022-02-22 23:54:38 +0300

    added new java class

--- src/main/java/gitPackage/GitPractise.java
+++ src/main/java/gitPackage/GitPractise.java
@@ -2,6 +2,6 @@ package gitPackage;
 
 public class GitPractise {
     public static void main(String[] args) {
-        System.out.println("This is Git practise");
+        System.out.println("This is Git practiseg");
     }
 }
